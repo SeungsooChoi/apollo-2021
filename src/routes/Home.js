06b4f51018de94a2ee5f99e188db5e95a1aae3ee
SeungsooChoi <tmdtn5389@gmail.com>
2021-08-26 12:14:15 +0900

    Feat : Add get movies query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1 key={m.id}>{m.id}</h1>);
+  }
+};
